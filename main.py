aac2fd4e017588f1195053a16d5a48dd403ae950
esavelliant14 <esa.freelance@gmail.com>
2025-12-31 21:38:55 +0700

    ada salah

--- main.py
+++ main.py
@@ -1048,63 +1048,61 @@ def checking_task():
                             </configuration>
                             """)
                             cfg_get = dev.rpc.get_config(filter_xml=filter_xml_check)
-                                rollback_config = f"""
-                                deactivate logical-systems {row.hostname} interfaces {row.interface} unit {row.unit_interface}
-                                deactivate logical-systems {row.hostname} interfaces {row.interface} unit {row.unit_interface}
-                                """
-                                cfg.load(rollback_config, format="set")
-                                cfg.commit()
-
-                                filter_xml = etree.XML(f"""
-                                <configuration>
-                                    <logical-systems>
-                                        <name>{row.hostname}</name>
-                                        <interfaces>
-                                            <interface>
-                                              <name>{row.interface}</name>
-                                                <unit>
-                                                    <name>{row.unit_interface}</name>
-                                                </unit>
-                                            </interface>
-                                      </interfaces>
-                                    </logical-systems>
-                                </configuration>
-                                """)
-                                cfg_get = dev.rpc.get_config(filter_xml=filter_xml)
-                                unit = cfg_get.find('.//unit')
-                                attr_unit = unit.get('inactive')
-                                if attr_unit:
-                                    # update status jadi Inactive
-                                    conn.execute(text("""
-                                        UPDATE table_bwm_bod SET status='Success'
-                                        WHERE id=:id
-                                    """), {"id": row.id})
-
-                                    results.append({
-                                        "hostname": row.hostname,
-                                        "interface": row.interface,
-                                        "unit": row.unit_interface,
-                                        "status": "Disable success"
-                                    })
-                                    conn.execute(
-                                        text("""
-                                                INSERT INTO table_loggings (action_by, category_action, status, ip_address, agent, details, id_group, created_at)
-                                                VALUES (:action_by, :category_action, :status, :ip_address, :agent, :details, :id_group, NOW())
-                                        """),
-                                        {
-                                            "action_by": "Automation System",
-                                            "category_action": "Disable client",
-                                            "status": "Success",
-                                            "ip_address": "localhost",
-                                            "agent": "backend",
-                                            "details": f"Disable client {row.description} interface={row.interface} unit={row.unit_interface} success, status change with Success"
-                                            "id_group": {row.id_group}
-                                        }
-                                    )
-                                else:
-                                    results.append({
-                                        "status": "failed",
-                                    })
+                            rollback_config = f"""
+                            deactivate logical-systems {row.hostname} interfaces {row.interface} unit {row.unit_interface}
+                            deactivate logical-systems {row.hostname} interfaces {row.interface} unit {row.unit_interface}
+                            """
+                            cfg.load(rollback_config, format="set")
+                            cfg.commit()
+                            filter_xml = etree.XML(f"""
+                            <configuration>
+                                <logical-systems>
+                                    <name>{row.hostname}</name>
+                                    <interfaces>
+                                        <interface>
+                                          <name>{row.interface}</name>
+                                            <unit>
+                                                <name>{row.unit_interface}</name>
+                                            </unit>
+                                        </interface>
+                                  </interfaces>
+                                </logical-systems>
+                            </configuration>
+                            """)
+                            cfg_get = dev.rpc.get_config(filter_xml=filter_xml)
+                            unit = cfg_get.find('.//unit')
+                            attr_unit = unit.get('inactive')
+                            if attr_unit:
+                                # update status jadi Inactive
+                                conn.execute(text("""
+                                    UPDATE table_bwm_bod SET status='Success'
+                                    WHERE id=:id
+                                """), {"id": row.id})
+                                results.append({
+                                    "hostname": row.hostname,
+                                    "interface": row.interface,
+                                    "unit": row.unit_interface,
+                                    "status": "Disable success"
+                                })
+                                conn.execute(
+                                    text("""
+                                            INSERT INTO table_loggings (action_by, category_action, status, ip_address, agent, details, id_group, created_at)
+                                            VALUES (:action_by, :category_action, :status, :ip_address, :agent, :details, :id_group, NOW())
+                                    """),
+                                    {
+                                        "action_by": "Automation System",
+                                        "category_action": "Disable client",
+                                        "status": "Success",
+                                        "ip_address": "localhost",
+                                        "agent": "backend",
+                                        "details": f"Disable client {row.description} interface={row.interface} unit={row.unit_interface} success, status change with Success"
+                                        "id_group": {row.id_group}
+                                    }
+                                )
+                            else:
+                                results.append({
+                                    "status": "failed",
+                                })
 
 
 
